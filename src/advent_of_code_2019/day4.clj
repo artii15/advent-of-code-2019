77be88a4c4b3cf9a99e232817e1fdf199f2b8b5e
Artii15 <artur.kowalski15@gmail.com>
2019-12-14 18:36:02 +0100

    code refactor

--- src/advent_of_code_2019/day4.clj
+++ src/advent_of_code_2019/day4.clj
@@ -15,22 +15,19 @@
          (recur next-elements groups (conj group next-element))
          (recur next-elements (conj groups group) [next-element]))))))
 
-(defn has-multiples? [elements]
-  (not (empty? (filter #(> (count %) 1) (get-groups elements)))))
+(defn has-appropriate-groups? [elements is-group-appropriate?]
+  ((comp not empty?) (filter is-group-appropriate? (get-groups elements))))
 
 (defn monothonic-with-multiples? [elements]
-  (and (monothonic? elements) (has-multiples? elements)))
+  (and (monothonic? elements) (has-appropriate-groups? elements #(> (count %) 1))))
 
 (defn part-1 []
   (let [passwords (map str (range 138307 654505))]
     (count 
      (filter monothonic-with-multiples? passwords))))
 
-(defn has-doubles? [elements]
-  (not (empty? (filter #(= (count %) 2) (get-groups elements)))))
-
 (defn monothonic-with-doubles? [elements]
-  (and (monothonic? elements) (has-doubles? elements)))
+  (and (monothonic? elements) (has-appropriate-groups? elements #(= (count %) 2))))
 
 (defn part-2 []
   (let [passwords (map str (range 138307 654505))]
