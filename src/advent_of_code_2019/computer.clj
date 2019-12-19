8e5faf1e2e6b37e036508264e138fb5aa67d24a1
Artii15 <artur.kowalski15@gmail.com>
2019-12-19 20:10:50 +0100

    computer prepared for loops

--- src/advent_of_code_2019/computer.clj
+++ src/advent_of_code_2019/computer.clj
@@ -6,7 +6,8 @@
     "0" (get memory param)
     "1" param))
 
-(defn two-param-compute-op [{memory :memory position :position stdout :stdout} params-modes op]
+(defn two-param-compute-op [{memory :memory position :position stdout :stdout stdin :stdin} 
+                            params-modes op]
   (let [param-1-mode (nth params-modes 0)
         param-2-mode (nth params-modes 1)
         param-1 (get memory (+ position 1))
@@ -16,7 +17,8 @@
         param-2-value (read-param-value memory param-2 param-2-mode)]
     {:memory (assoc memory result-pos (op param-1-value param-2-value))
      :position (+ position 4)
-     :stdout stdout}))
+     :stdout stdout
+     :stdin stdin}))
 
 (defn add [program-state params-modes]
   (two-param-compute-op program-state params-modes +))
@@ -24,22 +26,26 @@
 (defn multiply [program-state params-modes]
   (two-param-compute-op program-state params-modes *))
 
-(defn write-input [{memory :memory position :position stdout :stdout} _]
+(defn write-input [{memory :memory position :position stdout :stdout stdin :stdin} _]
   (let [result-pos (get memory (+ position 1))]
-    {:memory (assoc memory result-pos (Integer. (read-line)))
+    {:memory (assoc memory result-pos (peek stdin))
      :position (+ position 2)
-     :stdout stdout}))
+     :stdout stdout
+     :stdin (pop stdin)}))
 
-(defn print-mem [{memory :memory position :position stdout :stdout} params-modes]
+(defn print-mem [{memory :memory position :position stdout :stdout stdin :stdin} 
+                 params-modes]
   (let [param-mode (first params-modes)
         param (get memory (+ position 1))
         param-value (read-param-value memory param param-mode)]
     (println param-value)
     {:memory memory
      :position (+ position 2)
-     :stdout (conj stdout param-value)}))
+     :stdout (conj stdout param-value)
+     :stdin stdin}))
 
-(defn jump-if [{memory :memory position :position stdout :stdout} params-modes with-zero-comparator]
+(defn jump-if [{memory :memory position :position stdout :stdout stdin :stdin} 
+               params-modes with-zero-comparator]
   (let [param-1-mode (nth params-modes 0)
         param-2-mode (nth params-modes 1)
         param-1 (get memory (+ position 1))
@@ -48,7 +54,8 @@
         param-2-value (read-param-value memory param-2 param-2-mode)]
     {:memory memory
      :position (if (with-zero-comparator param-1-value 0) param-2-value (+ position 3))
-     :stdout stdout}))
+     :stdout stdout
+     :stdin stdin}))
 
 (defn jump-if-true [program-state params-modes]
   (jump-if program-state params-modes not=))
@@ -58,7 +65,8 @@
 
 (defn bool-to-int [bool-val]
   (if bool-val 1 0))
-(defn compare-and-store [{memory :memory position :position stdout :stdout} params-modes comparator]
+(defn compare-and-store [{memory :memory position :position stdout :stdout stdin :stdin} 
+                         params-modes comparator]
   (let [param-1-mode (nth params-modes 0)
         param-2-mode (nth params-modes 1)
         param-1 (get memory (+ position 1))
@@ -68,7 +76,8 @@
         result-pos (get memory (+ position 3))]
     {:memory (assoc memory result-pos (bool-to-int (comparator param-1-value param-2-value)))
      :position (+ position 4)
-     :stdout stdout}))
+     :stdout stdout
+     :stdin stdin}))
 
 (defn less-than [program-state params-modes]
   (compare-and-store program-state params-modes <))
@@ -100,12 +109,14 @@
         params-modes (concat (read-params-modes op-code) (repeat \0))]
     {:instruction-code instruction-code :params-modes params-modes}))
 
-(defn interpret-and-get-state [{memory :memory position :position :as program-state}]
+(defn interpret-and-get-state [{memory :memory position :position stdin :stdin :as program-state}]
   (let [{instruction-code :instruction-code
          params-modes :params-modes} (read-op-code (get memory position))]
-    (if (= instruction-code 99) program-state
-        (let [update-state (get instructions instruction-code)]
-          (recur (update-state program-state params-modes))))))
+    (cond 
+      (or (= instruction-code 99)
+          (and (= instruction-code 3) (empty? stdin))) program-state
+      :else (let [update-state (get instructions instruction-code)]
+              (recur (update-state program-state params-modes))))))
 
 (defn interpret-program [program-state]
   (let [final-state (interpret-and-get-state program-state)]
@@ -117,7 +128,8 @@
         op-codes-indices (range 0 (count int-op-codes))]
      {:memory (zipmap op-codes-indices int-op-codes)
       :position 0
-      :stdout []}))
+      :stdout clojure.lang.PersistentQueue/EMPTY
+      :stdin clojure.lang.PersistentQueue/EMPTY}))
 
 (defn interpret-from-string-and-get-state [program-string]
   (interpret-and-get-state (initialize-state-from-string program-string)))
@@ -128,3 +140,16 @@
 
 (defn halted? [{memory :memory position :position}]
   (= (read-op-code (get memory position)) 99))
+
+(defn waits-for-input? [{memory :memory position :position stdin :stdin}]
+  (and (empty? stdin) 
+       (= (read-op-code (get memory position)) 3)))
+
+(defn get-output [program-state]
+  (let [stdout (get program-state :stdout)]
+    {:output (peek stdout) 
+     :new-program-state (assoc program-state :stdout (pop stdout))}))
+
+(defn add-input [program-state input-value]
+  (let [stdin (get program-state :stdin)]
+    (assoc program-state :stdin (conj stdin input-value))))
--- src/advent_of_code_2019/day7.clj
+++ src/advent_of_code_2019/day7.clj
@@ -1,30 +1,45 @@
 (ns advent-of-code-2019.day7
-  (:require [clojure.string :as str]
-            [clojure.math.combinatorics :as combo]
+  (:require [clojure.math.combinatorics :as combo]
             [advent-of-code-2019.computer :as computer]
             [advent-of-code-2019.files :as files]))
 
-(defn run-amplifier [amplifier-state phase-setting input-signal]
-  (let [stdin (str phase-setting "\n" input-signal)]
-    (with-in-str stdin (computer/interpret-and-get-state amplifier-state))))
+(defn run-amplifier [amplifier input]
+  (let [amplifier-with-input (computer/add-input amplifier input)]
+    (computer/interpret-and-get-state amplifier-with-input)))
 
-(defn update-amplifiers-states [[signal states] [amplifier-state phase-setting]]
-  (let [new-amplifier-state (run-amplifier amplifier-state phase-setting signal)]
-    [(peek (get new-amplifier-state :stdout)) 
-     (conj states (dissoc new-amplifier-state :stdout))]))
+(defn run-next-amplifier [ran-amplifiers next-amplifier]
+  (let [last-ran-amplifier (peek ran-amplifiers)
+        {new-last-amplifier-state :new-program-state last-output :output} (computer/get-output last-ran-amplifier)
+        next-ran-amplifier (run-amplifier next-amplifier last-output)
+        last-amplifier-index (- (count ran-amplifiers) 1)
+        ran-amplifiers-with-last-updated (assoc ran-amplifiers last-amplifier-index new-last-amplifier-state)]
+    (conj ran-amplifiers-with-last-updated next-ran-amplifier)))
 
-(defn run-phase [amplifiers-states phase-settings initial-signal]
-  (reduce update-amplifiers-states 
-          [initial-signal []] 
-          (map vector amplifiers-states phase-settings)))
+(defn run-phase 
+  ([amplifiers] (run-phase amplifiers 0))
+  ([amplifiers initial-signal]
+   (reduce run-next-amplifier 
+           [(run-amplifier (first amplifiers) initial-signal)] 
+           (rest amplifiers))))
 
-(defn generate-possible-phase-settings [number-of-amplifiers]
-  (combo/permutations (range 0 number-of-amplifiers)))
+(defn generate-possible-phase-settings [min-setting max-setting]
+  (combo/permutations (range min-setting (inc max-setting))))
+
+(defn generate-amplifiers [amplifier-program phase-settings]
+  (reduce #(conj %1 (computer/add-input amplifier-program %2)) [] phase-settings))
+
+(defn read-run-phase-result [amplifiers]
+  (let [last-amplifier (peek amplifiers)]
+    (get (computer/get-output last-amplifier) :output)))
 
 (defn part-1 [file-path]
   (let [code (files/file-to-string file-path)
-        initial-amplifiers-states (repeat 5 (computer/initialize-state-from-string code))]
-    (run-phase initial-amplifiers-states [0 1 2 3 4] 0)))
+        amplifier-program (computer/initialize-state-from-string code)
+        phase-settings-combos (generate-possible-phase-settings 0 4)
+        all-amplifiers-seqs-set (map #(generate-amplifiers amplifier-program %) phase-settings-combos)
+        all-amplifiers-seqs-after-run (map run-phase all-amplifiers-seqs-set)
+        all-runs-results (map read-run-phase-result all-amplifiers-seqs-after-run)]
+    (apply max all-runs-results)))
 
 (comment
   (part-1 "/home/artur/Pulpit/advent-inputs/day7-input"))
